9abccbc8fe121913c6c4064770faea7fdfea59b6
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-11-02 12:21:33 -0300

    add singleton resource para cozinha

--- src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -6,9 +6,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 
+@Component
 public class EstadoRepositoryImpl implements EstadoRepository{
     @PersistenceContext
     private EntityManager manager;
