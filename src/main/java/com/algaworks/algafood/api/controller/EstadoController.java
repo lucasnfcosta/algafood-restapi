655bc70e635eef05bcd88405efc2a598c6f56e23
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-11-15 20:29:46 -0300

    add adição de estados

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -3,12 +3,16 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
+import com.algaworks.algafood.domain.service.CadastroEstadoService;
 
 @RestController
 @RequestMapping("/estados")
@@ -17,8 +21,18 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
+    @Autowired
+    private CadastroEstadoService cadastroEstado;
+
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.listar();
+    }
+
+    @PostMapping
+    public ResponseEntity<Estado> adicionar(@RequestBody Estado estado) {
+        Estado estadoSalvo = cadastroEstado.salvar(estado);
+
+        return ResponseEntity.ok(estadoSalvo);
     }
 }
