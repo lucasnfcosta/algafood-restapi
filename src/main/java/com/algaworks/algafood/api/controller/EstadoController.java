277767d11a09e7fb47415818b24f9ba6651d4b25
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-11-15 20:46:03 -0300

    add método de atualização de estados

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -2,10 +2,13 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +37,19 @@ public class EstadoController {
         Estado estadoSalvo = cadastroEstado.salvar(estado);
 
         return ResponseEntity.ok(estadoSalvo);
+    }
+
+    @PutMapping("/{estadoId}")
+    public ResponseEntity<?> atualizar(@PathVariable("estadoId") Long id, @RequestBody Estado estado) {
+        Estado estadoAtual = estadoRepository.buscar(id);
+
+        if (estadoAtual != null) {
+            BeanUtils.copyProperties(estado, estadoAtual, "id");
+
+            estadoAtual = cadastroEstado.salvar(estadoAtual);
+            return ResponseEntity.ok(estadoAtual);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
