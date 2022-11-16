9c0c9697a5c30734c51585dd471bddf49b522a30
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-11-15 22:16:58 -0300

    add métodos de listagem e adição de cidades

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -2,7 +2,6 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoExisteException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+
+@Service
+public class CadastroCidadeService {
+    
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public Cidade salvar(Cidade cidade) {
+        Long estadoId = cidade.getEstado().getId();
+        Estado estado = estadoRepository.buscar(estadoId);
+
+        if (estado == null) {
+            throw new EntidadeNaoExisteException(String.format("Estado com o id %d não existe", estadoId));
+        }
+
+        return cidadeRepository.salvar(cidade);
+        
+    }
+}
