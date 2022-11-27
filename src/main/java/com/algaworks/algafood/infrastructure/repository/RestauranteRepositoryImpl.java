8dd49e2b69b38390c0bcb3c0b9c3d3a0ef01107e
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-11-27 11:54:51 -0300

    add consulta dinâmica para restaurantes

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
+    
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+        StringBuilder jpql = new StringBuilder();
+        jpql.append("from Restaurante where 0 = 0 ");
+
+        Map<String, Object> parametros = new HashMap<String, Object>();
+
+        if (StringUtils.hasLength(nome)) {
+            jpql.append("and nome like :nome ");
+            parametros.put("nome", "%" + nome + "%");
+        }
+
+        if (taxaFreteFinal != null) {
+            jpql.append("and taxaFreteInicial >= :taxaFreteInicial ");
+            parametros.put("taxaFreteInicial", taxaFreteInicial);
+        }
+
+        if (taxaFreteFinal != null) {
+            jpql.append("and taxaFreteFinal <= :taxaFreteFinal ");
+            parametros.put("taxaFreteFinal", taxaFreteFinal);
+        }
+
+        TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+        
+        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+
+        return query.getResultList();
+    }
+}
